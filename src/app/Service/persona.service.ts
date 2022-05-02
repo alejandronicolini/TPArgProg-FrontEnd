7cfff9687e50b7266250ae918e3a9a13e07a6d8d
Alejandro Nicolini <alenicolini139@hotmail.com>
2022-05-01 23:13:47 -0300

    commit prodccion

--- src/app/Service/persona.service.ts
+++ src/app/Service/persona.service.ts
@@ -1,5 +1,5 @@
-import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Persona } from '../Modelo/Persona';
 
